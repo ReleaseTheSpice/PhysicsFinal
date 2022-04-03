04ab858c7f2c83424ab88863cf2dcbe5720deff7
JackyLau01 <jlau154@my.bcit.ca>
2022-04-03 15:47:51 -0700

    added le hoop

--- Assets/Spin.cs
+++ Assets/Spin.cs
@@ -20,10 +20,12 @@ public class Spin : MonoBehaviour
     
     [Header("Angular Velocity")]
     public float angularVelocity;
-    
+
+    public Vector3 scale;
     // Start is called before the first frame update
     void Start()
     {
+        scale = transform.localScale;
         if (shapeConstant == 0.0f)
         {
             shapeConstant = 1.0f;
@@ -35,7 +37,7 @@ public class Spin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localScale = new Vector3(radius, transform.localScale.y, radius);
+        transform.localScale = new Vector3(scale.x * radius, transform.localScale.y, scale.z * radius);
         momentOfInertia = shapeConstant * mass * radius * radius;
         angularVelocity = angularMomentum / momentOfInertia;
         transform.Rotate(0.0f, angularVelocity * Time.deltaTime, 0.0f, Space.Self);
